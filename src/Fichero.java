82d0261d6be7f4e7ce7fbee25d883336e1bd9f1f
“jother043” <josemgh166@gmail.com>
2023-10-05 21:13:46 +0200

    Actualizaciones de verificaciones de bytes de los campos.

--- src/Fichero.java
+++ src/Fichero.java
@@ -9,9 +9,9 @@ public class Fichero {
     private static List<Coche> cocheList;
     private final String fileName;
     private final String campoClave;
-    public static final int BYTES_MATRICULA = 7;
-    public static final int BYTES_MODELO = 32;
-    public static final int BYTES_MARCA = 32;
+    public static final byte BYTES_MATRICULA = 7;
+    public static final byte BYTES_MODELO = 32;
+    public static final byte BYTES_MARCA = 32;
     private int longitudRegistro;
     private long numeroRegistros;
     private final int NUMBER_OF_BYTES = 71;
@@ -51,98 +51,56 @@ public class Fichero {
 
     }
 
+
     /**
-     * Recupera un registro del fichero a partir de su clave.
-     *
-     * @param clave Clave del registro que queremos recuperar.
-     * @return Map con los datos del registro.
+     * Inserta un coche en el fichero.
+     * Si el coche ya existe no lo inserta.
+     * Si la posición es menor que 0 lanza una excepción.
+     * Si el número de bytes es menor que 0 o mayor que la longitud del registro lanza una excepción.
+     * Si el coche es null lanza una excepción.
+     * Si la longitud de la matrícula es mayor que 7 lanza una excepción.
+     * Si la longitud del modelo es mayor que 32 lanza una excepción.
+     * Si la longitud de la marca es mayor que 32 lanza una excepción.
+     * Si no se cumple ninguna de las condiciones anteriores inserta el coche en el fichero.
+     * @param coche
+     * @param posicion
+     * @param numeroBytes
      * @throws IOException
-     */
-   // public Map<String, String> recuperar(String clave) throws IOException {
-
-//        int posicion = 0;
-//        boolean encontrado = false;
-//        Map<String, String> resultado;
-//        //Try with resources para cerrar el fichero.
-//        try (FileInputStream fileInputStream = new FileInputStream(this.fileName)) {
-//            resultado = null;
-//            //Mientras no lleguemos al final del fichero y no hayamos encontrado el registro.
-//            while (posicion < numeroRegistros && !encontrado) {
-//                //Creamos un array de bytes con la longitud del registro.
-//                byte[] registro = new byte[this.longitudRegistro];
-//                /*
-//                Si el número de bytes leidos es menor que la longitud del registro es que hemos llegado al final del fichero.
-//                Por lo que devolvemos null.
-//                 */
-//                if (fileInputStream.read(registro, 0, this.longitudRegistro) < this.longitudRegistro) {
-//                    return null;
-//                }
-//
-//                int posicionCampo = 0;
-//                String valorCampo = null;
-//                /*
-//                Recorremos el mapa de datos para recuperar los campos del registro y si el campo actual es el campo clave
-//                comprobamos si es el registro que estamos buscando y si es así lo devolvemos.
-//                 */
-//                for (Map.Entry<String, Integer> campo : datos.entrySet()) {
-//                    String campoActual = campo.getKey();
-//                    int longitudCampo = campo.getValue();
-//                    //Si el campo actual es el campo clave lo recuperamos.
-//                    if (campoActual.equals(campoClave)) {
-//                        valorCampo = new String(registro, posicionCampo, longitudCampo, StandardCharsets.UTF_8);
-//                        break;
-//                    }
-//                    //Incrementamos la posición del campo.
-//                    posicionCampo += longitudCampo;
-//                }
-//
-//                if (clave.equals(valorCampo)) {
-//                    resultado = new HashMap<>();
-//                    posicionCampo = 0;
-//                    //Recorremos el mapa de datos para recuperar los campos del registro.
-//                    for (Map.Entry<String, Integer> campo : datos.entrySet()) {
-//                        String campoActual = campo.getKey();
-//                        int longitudCampo = campo.getValue();
-//                        String valor = new String(registro, posicionCampo, longitudCampo, StandardCharsets.UTF_8);
-//                        resultado.put(campoActual, valor);
-//                        //Incrementamos la posición del campo.
-//                        posicionCampo += longitudCampo;
-//                    }
-//                    encontrado = true;
-//                }
-//            }
-//            return resultado;
-//        }
-//    }
-
-    /**
-     * Inserta un registro en el fichero en un lugar concreto del fichero teniendo en cuenta
-     * los bytes de los registros 7 bytes para matrícula, 32 bytes para modelo y 32 bytes para marca.
      */
     public void insetar(Coche coche,int posicion,int numeroBytes) throws IOException {
 
 
-        if(posicion < 0){
+        if(posicion < 0 ){
             throw new IllegalArgumentException("La posición no es válida");
         }else if(numeroBytes < 0 || numeroBytes > longitudRegistro){
             throw new IllegalArgumentException("El número de bytes no es válido");
         }else if(coche == null){
             throw new IllegalArgumentException("El coche no puede ser null");
-        }else if(coche.getTuition().length() > BYTES_MATRICULA){
-            throw new IllegalArgumentException("La matrícula debe tener 7 bytes");
-        }else if(coche.getModel().length() > BYTES_MODELO){
-            throw new IllegalArgumentException("El modelo debe tener 32 bytes");
-        } else if (coche.getBrand().length() > BYTES_MARCA) {
-            throw new IllegalArgumentException("La marca debe tener 32 bytes");
         }
+        //Comprobamos que la longitud de los campos no sea mayor que la longitud de los bytes.
+        if(coche.getTuition().getBytes().length > BYTES_MATRICULA) {
+            throw new IllegalArgumentException("La matrícula no puede tener más de 7 bytes");
+        }else if(coche.getModel().getBytes().length > BYTES_MODELO){
+            throw new IllegalArgumentException("El modelo no puede tener más de 32 bytes");
+        }else if(coche.getBrand().getBytes().length > BYTES_MARCA){
+            throw new IllegalArgumentException("La marca no puede tener más de 32 bytes");
+        }else{
+            //si no es mayor lo rellenamos con espacios en blanco.
+            coche.setTuition(String.format("%-7s", coche.getTuition()));
+            coche.setModel(String.format("%-32s", coche.getModel()));
+            coche.setBrand(String.format("%-32s", coche.getBrand()));
+        }
+
+        //Si existe el coche no lo insertamos.
         if(existeCoche(coche)) {
             System.err.println("El coche ya existe");
         }else{
             RandomAccessFile raf = new RandomAccessFile(fileName, "rw");
+            //Nos posicionamos en la posición que queremos insertar el registro.
             raf.seek((long) posicion * longitudRegistro);
             raf.write(coche.getTuition().getBytes());
-            raf.write(coche.getModel().getBytes());
             raf.write(coche.getBrand().getBytes());
+            raf.write(coche.getModel().getBytes());
             raf.close();
             //Lo utilizamos para saber lo que están repetidos.
             cocheList.add(coche);
@@ -171,18 +129,20 @@ public class Fichero {
 
         try {
             Map<String, Integer> datos = new HashMap<>();
-            datos.put("matricula", BYTES_MATRICULA);
-            datos.put("modelo", BYTES_MODELO);
-            datos.put("marca", BYTES_MARCA);
-            Fichero fichero = new Fichero("coches.dat", datos, "matricula");
+            datos.put("matricula", (int) BYTES_MATRICULA);
+            datos.put("modelo", (int) BYTES_MODELO);
+            datos.put("marca", (int) BYTES_MARCA);
+            Fichero fichero = new Fichero("coches.csv", datos, "matricula");
             Coche coche = new Coche("Seat", "Ibiza", "1234ABC");
             fichero.insetar(coche, 0, fichero.NUMBER_OF_BYTES);
-            Coche coche2 = new Coche("Seat", "Ibiza", "1234ABC");
+            Coche coche2 = new Coche("Seat", "Arona", "2345ABC");
             fichero.insetar(coche2, 1, fichero.NUMBER_OF_BYTES);
             Coche coche3 = new Coche("Seat", "Leon", "1684ABC");
             fichero.insetar(coche3, 2, fichero.NUMBER_OF_BYTES);
             Coche coche4 = new Coche("Seat", "Ateca", "6984ABC");
             fichero.insetar(coche4, 3, fichero.NUMBER_OF_BYTES);
+            Coche coche5 = new Coche("Citroen", "Xsara", "3546LHF");
+            fichero.insetar(coche5, 4, fichero.NUMBER_OF_BYTES);
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
